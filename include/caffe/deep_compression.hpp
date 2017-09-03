f11cd16aefb98166902059ca66bbe7841a85a1e2
wanghuan <21631068@zju.edu.cn>
2017-09-03 20:00:00 +0800

    rm some useless items in deep_compression.cpp/hpp

--- include/caffe/deep_compression.hpp
+++ include/caffe/deep_compression.hpp
@@ -30,33 +30,23 @@ public:
     
     static int step_;
     static float PruneRate[100];
-    static float prune_ratio[100];
     static int num_pruned_column[100];
     static int num_pruned_row[100];
     static bool IN_TEST;
     static bool IN_RETRAIN;
-    static string Method;
+
     
     static int window_size;
         
     static float score_decay_rate;
     static bool use_score_decay;
-    static int num_of_col_to_prune_per_time;
-    static int num_row_once_prune;
     
     // When to Prune or Reg etc.
-    static int period;
-    static int when_to_apply_masks;
     static int when_to_col_reg;
     static float col_reg;
     static float diff_reg;
     
     // Decrease-Weight_Decay
-    static int when_to_dwd;
-    static int dwd_end_iter;
-    static float wd_end;
-    static string dwd_mode;
-    static int dwd_step;
     static int max_num_column_to_prune;
 
     // Selective Reg
--- src/caffe/deep_compression.cpp
+++ src/caffe/deep_compression.cpp
@@ -22,7 +22,6 @@ namespace caffe {
     int DeepCompression::num_pruned_row[100] = {0, 0, 0, 0, 0};
     bool DeepCompression::IN_TEST = false;
     bool DeepCompression::IN_RETRAIN = false;
-    string DeepCompression::Method = "PFilter"; // "SPP", "PPruning", "PFilter"
     
 
     // use window proposal or score decay
@@ -39,25 +38,15 @@ namespace caffe {
     float DeepCompression::diff_reg = 0.00001; 
         
     // Decrease-Weight-Decay
-    const int begin_time = -1;
-    int DeepCompression::when_to_dwd = begin_time; // when to decrease weight decay
-    int DeepCompression::dwd_end_iter = 150000;
-    float DeepCompression::wd_end = 0.175; // decrease weight decay to, say, 20%
-    string DeepCompression::dwd_mode = "step_linearly"; // "linealy, step_linearly, adaptive, None". If "None" used, don't decrease weight decay.
-    int DeepCompression::dwd_step = 100; // If "step_linearly" used, this must be provided.
     int DeepCompression::max_num_column_to_prune = 0; // If "adaptive" used, this must be provided.
     
     // When to Prune or Reg etc.
-    int DeepCompression::period = 7654321; // a hyper-parameters for paper of IHT (Iterative Hard Thresholding), used for iterative training
     int DeepCompression::when_to_col_reg = 7654321; // when to apply col reg, SSL or SelectiveReg
-    int DeepCompression::when_to_apply_masks = begin_time; // To determine when masks work. -1: always apply masks
     
     
     // PruneRate etc.
     float DeepCompression::PruneRate[100] = {0.75, 0.75, 0.75, 0.75, 0.75};
-    float DeepCompression::prune_ratio[100] = {0.75, 0.75, 0.75, 0.75, 0.75};
-    int DeepCompression::num_of_col_to_prune_per_time = 1;  // the number of columns to prune per time
-    int DeepCompression::num_row_once_prune = 2; // the "m" parameter in paper "Pruning Filter"
+
     
     // Adaptive SPP
     float DeepCompression::loss = 0;
